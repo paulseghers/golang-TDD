297ef72d2df7472e636c2c579c2ed2aa822599d9
paulseghers <0099seghers@gmail.com>
2023-01-04 17:45:25 +0100

    exercise 4

--- 3-array-testing/sum.go
+++ 3-array-testing/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var res []int
+	for _, l := range numbersToSum {
+		res = append(res, Sum(l))
+	}
+	return res
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var res []int
+	for _, l := range numbersToSum {
+		if len(l) == 0 {
+			res = append(res, 0)
+		} else {
+			tail := l[1:]
+			res = append(res, Sum(tail))
+		}
+	}
+	return res
+}
--- 3-array-testing/sum_test.go
+++ 3-array-testing/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -19,5 +22,38 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func compareValues(t *testing.T, got []int, want []int) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		compareValues(t, got, want)
+	})
+
+	t.Run("safely check passing of empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		compareValues(t, got, want)
 	})
 }
