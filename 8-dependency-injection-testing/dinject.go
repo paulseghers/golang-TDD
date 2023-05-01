0edaf9f073a733f3297808b0caed1575ab75ca0c
paulseghers <0099seghers@gmail.com>
2023-05-01 22:08:23 +0200

    started 8

---
+++ 8-dependency-injection-testing/dinject.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func httpResGreeter(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Quandale dingle")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(httpResGreeter)))
+}
