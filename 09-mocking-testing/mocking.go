dc50d7a65d1fcfecd84293f3586c5c927649bde0
paulseghers <0099seghers@gmail.com>
2024-01-19 22:44:38 +0100

    fixed test and learned mocking

--- 09-mocking-testing/mocking.go
+++ 09-mocking-testing/mocking.go
@@ -1,5 +1,40 @@
 package main
 
-func main(){
-	Countdown()
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int //return type of function that will be implemented
+}
+
+type DefaultSleeper struct {
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i) //needs buffer location
+		sleeper.Sleep()      //we dont decide the sleep time in the function anymore
+	}
+	fmt.Fprint(out, "Go!")
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- 09-mocking-testing/mocking_test.go
+++ 09-mocking-testing/mocking_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{} //read incoming stuff
-
-	Countdown(buffer)
+	sleeper := &SpySleeper{}
+	Countdown(buffer, sleeper)
 
 	got := buffer.String() //returns the whole chungus from the byte slice that this buffer points to/"implements"(?)
 	want := `3
-	2
-	1
-	Go!`
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
