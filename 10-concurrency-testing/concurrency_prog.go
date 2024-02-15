0af7a1eb201cf229c6ed1288b011e5a952847070
paulseghers <0099seghers@gmail.com>
2024-02-15 23:57:00 +0100

    getting spicy

--- 10-concurrency-testing/concurrency_prog.go
+++ 10-concurrency-testing/concurrency_prog.go
@@ -1,1 +1,11 @@
 package main
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	res := make(map[string]bool)
+	for _, url := range urls {
+		res[url] = wc(url)
+	}
+	return res
+}
--- 10-concurrency-testing/concurrency_test.go
+++ 10-concurrency-testing/concurrency_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsite(b testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "http://a.url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
